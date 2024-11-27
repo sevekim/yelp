58ab6d37a503676b2618899e4ac95417c5b2a5c5
steve <sevek.im@gmail.com>
2024-11-26 23:24:35 -0700

    Add site_constants.dart to .gitignore

--- lib/core/constants/app_constant/site_constants.dart
+++
@@ -1,4 +0,0 @@
-String apiKey =
-    'cA6AsgYtTp96t-3XEF5VXQyHGF_GOY9hgl_XqXvtUHPkHkIL5hlXQ3sUyhb4A5uNu2rjNmK-iOXvjWsYVfIfmerXhO8OY7qx9pQmm7CByEyd5ea8lZl_9XDv5kVGZ3Yx';
-
-String defaultCity = 'calgary';
